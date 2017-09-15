3340a378ce3d595d07c14d9f1c2084f9e3061edb
Luc Cary <luc.cary@gmail.com>
2017-09-15 10:41:29 -0400

    update bash scripts, add new test cpp file

---
+++ mdy.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string day, month, year, date;
+
+    month = "03";
+    day = "04";
+    year = "06";
+    date = month + day + year;
+    cout << date << endl;
+
+    return 0;
+}
