0d39a2e2670df42c89491f295ac159494299348b
Luc Cary <luc.cary@gmail.com>
2017-09-15 10:27:59 -0400

    Test projects

---
+++ helloworld.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello world\n";
+    cout << "foo\n";
+    cout << "bar\t";
+    return 0;
+}
---
+++ sum.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n1, n2;
+    cout << "Enter two whole numbers separated by a space: ";
+    cin >> n1 >> n2;
+    cout << "Sum:\n";
+    cout << (n1 + n2) << endl;
+    return 0;
+}
