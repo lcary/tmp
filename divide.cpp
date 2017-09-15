2a822ca8033c2230f9c3a4b3048521b43ab538fe
Luc Cary <luc.cary@gmail.com>
2017-09-15 10:44:34 -0400

    update scripts and sources

--- divide.cpp
+++ divide.cpp
@@ -4,8 +4,9 @@ using namespace std;
 
 int main() {
     int a, b;
+    cout << "Enter two numbers:\n";
     cin >> a >> b;
-    cout << a / b << endl;
-    cout << a % b << endl;
+    cout << a << " / " << b << ": " << a / b << endl;
+    cout << a << " % " << b << ": " << a % b << endl;
     return 0;
 }
