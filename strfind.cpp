60a05b82164b83de5cb064b13308e53d84064dc3
Luc Cary <luc.cary@gmail.com>
2017-10-05 13:38:14 -0400

    add index to str.find() program

--- strfind.cpp
+++ strfind.cpp
@@ -3,20 +3,33 @@
 
 using namespace std;
 
+const int MAX_WORDS = 100;
+
 string readText();
 
-int countSymbolsWhile(string text, char symbol);
+int readWordIndex();
 
-int countSymbolsRecursively(string text, char symbol);
+int countSymbolsWhile(string text, string words[], char symbol);
 
-int countSymbolsRecursively(string text, int start, char symbol, int &count);
+int countSymbolsRecursively(string text, string words[], char symbol);
 
-int countWords(string text);
+int countSymbolsRecursively(string text, string words[], int start, char symbol, int &count);
+
+int countWords(string text, string words[]);
 
 int main() {
+    int index;
+    string words[MAX_WORDS];
+
+    // count and index words
     string text = readText();
-    int words = countWords(text);
-    cout << words << " words were found.\n";
+    int count = countWords(text, words);
+    cout << count << " words were found.\n";
+
+    // read word request and display word
+    index = readWordIndex();
+    cout << "Here's your word: " << words[index] << "\n";
+
     return 0;
 }
 
@@ -24,54 +37,81 @@ string readText()
 {
     string text;
 
-    cout << "Please enter text (separated by spaces): ";
+    cout << "Please enter no more than " << MAX_WORDS << " words (separated by spaces): ";
     getline(cin, text);
 
     return text;
 }
 
-int countSpaces(string text)
+int readWordIndex()
 {
-    return countSymbolsRecursively(text, ' ');
+    int n;
+    cout << "Enter index (0 = first) of word you want printed: ";
+    cin >> n;
+    return n;
 }
 
-int countSymbolsWhile(string text, char symbol)
+int countSpaces(string text, string words[])
 {
-    int counter = -1;
-    int currIndex = -1;
-    do
+    return countSymbolsWhile(text, words, ' ');
+}
+
+int countSymbolsWhile(string text, string words[], char symbol)
+{
+    bool endOfSentence = false;
+    int count = 0;
+    int start = 0;
+    int currIndex;
+    while (endOfSentence == false)
     {
-        currIndex++;
-        currIndex = text.find(' ', currIndex);
-        counter++;
+        currIndex = text.find(symbol, currIndex);
+        if (currIndex == string::npos)
+        {
+            string word = text.substr(start, (text.size() - currIndex));
+            endOfSentence = true;
+            cout << "Indexing " << word << " at index " << count << endl;
+            words[count] = word;
+        }
+        else
+        {
+            string word = text.substr(start, (currIndex - start));
+            cout << "Indexing " << word << " at index " << count << endl;
+            words[count] = word;
+            currIndex++;
+            start = currIndex;
+            count++;
+        }
     }
-    while (currIndex != string::npos);
-    return counter;
+    return count;
 }
 
-int countSymbolsRecursively(string text, char symbol)
+int countSymbolsRecursively(string text, string words[], char symbol)
 { 
     int start = 0;
     int count = 0;
-    return countSymbolsRecursively(text, start, symbol, count);
+    return countSymbolsRecursively(text, words, start, symbol, count);
 }
 
-int countSymbolsRecursively(string text, int start, char symbol, int &count)
+int countSymbolsRecursively(string text, string words[], int start, char symbol, int &count)
 {
     int currIndex = text.find(symbol, start);
     if (currIndex == string::npos)
+    {
+        words[count] = text.substr(start, text.size());
         return count;
+    }
     else
     {
+        words[count] = text.substr(start, (currIndex - start));
         currIndex++;
         count++;
-        return countSymbolsRecursively(text, currIndex, symbol, count);
+        return countSymbolsRecursively(text, words, currIndex, symbol, count);
     }
 }
 
-int countWords(string text)
+int countWords(string text, string words[])
 {
-    int spaces = countSpaces(text);
+    int spaces = countSpaces(text, words);
     // assume 1 more word than number of spaces
     return spaces + 1;
 }
